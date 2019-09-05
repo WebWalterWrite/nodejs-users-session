d970bdb1eae0d5f6a1aaa46b108d35df067e34d2
WebWalterWrite <webwalterwhrite@gmail.com>
2019-09-05 20:12:16 +0200

    🔧 add config routes

---
+++ routes/config.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+router.use((req, res, next) => {
+
+	res.header("Access-Control-Allow-Origin", req.headers.origin);
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, Content-Type, Accept, Authorization, X-Requested-With, x-auth-token"
+	);
+	res.header(
+		"Access-Control-Allow-Methods",
+		"GET,HEAD,POST,PUT,DELETE,OPTIONS"
+	);
+	res.header("Access-Control-Allow-Credentials", "true");
+	if ('OPTIONS' === req.method) {
+    res.sendStatus(200);
+} else {
+    next();
+}
+});
+
+module.exports = router;
