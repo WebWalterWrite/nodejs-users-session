c29acf2caf52dbfd634bffac6109cc2ff9b013b7
WebWalterWrite <webwalterwhrite@gmail.com>
2019-09-07 09:16:37 +0200

    ✨ add logout route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,11 @@
 const router = require('./config.js');
 const isAuthenticated = require('../controllers/user.controller').isAuthenticated;
 const getUserProfile = require ('../controllers/user.controller').getUserProfile;
-
+const destroyUserSession = require ('../controllers/user.controller').userDestroySession;
 
 router.get('/user-session', isAuthenticated, getUserProfile);
+
+router.get("/user-session/log-out", isAuthenticated, destroyUserSession);
+
 
 module.exports = router;
